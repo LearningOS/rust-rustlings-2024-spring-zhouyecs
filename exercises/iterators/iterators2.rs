2acc3df766d5e9b8c1bd3ee5660d4aa1181b9d58
Zhou Ye <zhouye@stu.pku.edu.cn>
2024-04-15 00:31:08 +0800

    finish iterators exercise

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -24,7 +22,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut ans = vec![];
+    for word in words {
+        ans.push(capitalize_first(word));
+    }
+    ans
 }
 
 // Step 3.
@@ -32,7 +34,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut ans = String::new();
+    for word in words {
+        ans.push_str(capitalize_first(word).as_str());
+    }
+    ans
 }
 
 #[cfg(test)]
